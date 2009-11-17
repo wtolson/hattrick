16d3e21e2d8a36c08eb6bf3f6e854adef70a6154
wtolson <wtolson@45e6a3c2-d350-11de-b7d7-53e004a27ad6>
2009-11-17 08:46:54 +0000

    Fixed (I think) the jacobian matrix and changed to bs integration.

--- hattrick.c
+++ hattrick.c
@@ -58,23 +58,23 @@ func (double t, const double y[], double f[],
 	}
 	return GSL_SUCCESS;
 }
-/*
+
 int
 jac (double t, const double y[], double *dfdy, 
      double dfdt[], void *params)
 {
 	double * M = (double *) params;
 	gsl_matrix_view dfdy_mat 
-		= gsl_matrix_view_array (dfdy, 6*N, 6*N);
+		= gsl_matrix_view_array (dfdy, 8*N, 8*N);
 	gsl_matrix * m = &dfdy_mat.matrix;
 	
 	
 	int i,j,k;
-	for (i=0; i<6*N; i++)
+	for (i=0; i<8*N; i++)
 	{
-		for (j=0; j<6*N; j++)
+		for (j=0; j<8*N; j++)
 		{
-			if ((i%2) == 0)
+			if ((i%8%2) == 0)
 			{
 				if (i+1 == j)
 				{
@@ -83,49 +83,54 @@ jac (double t, const double y[], double *dfdy,
 					gsl_matrix_set (m, i, j, 0.0);
 				}
 			} else {
-				if ((j%2) == 1) {
+				if ((j%8%2) == 1) {
 					gsl_matrix_set (m, i, j, 0.0);
 				} else if (i == j+1){
 					double ftemp = 0;
-					for(k=(i%6); k<6*N; k+=6)
+					for(k=(i%8); k<8*N; k+=8)
 					{
-						if (i/6!=k/6)
+						if (i/8!=k/8)
 						{
-							double r = sqrt(
-											(y[6*(k/6)] - y[6*(i/6)]) * (y[6*(k/6)] - y[6*(i/6)]) +
-											(y[6*(k/6)+2] - y[6*(i/6)+2]) * (y[6*(k/6)+2] - y[6*(i/6)]+2) +
-											(y[6*(k/6)+4] - y[6*(i/6)+4]) * (y[6*(k/6)+4] - y[6*(i/6)]+4) );
-							ftemp -= 3 * M[j/6] * (y[k-1]-y[i-1]) * (y[k-1]-y[i-1]) / (r*r*r*r*r);
-							ftemp += M[j/6] * y[i-1] / (r*r*r);
+							double x[3];
+							x[0] = y[8*(k/8)] - y[8*(i/8)];
+							x[1] = y[8*(k/8)+3] - y[8*(i/8)+3];
+							x[2] = y[8*(k/8)+6] - y[8*(i/8)+6];
+							double r = sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
+							
+							ftemp -= 3 * M[j/8] * (y[k-1]-y[i-1]) * (y[k-1]-y[i-1]) / (r*r*r*r*r);
+							ftemp += M[j/8] * y[i-1] / (r*r*r);
 						}
 					}					
 					gsl_matrix_set (m, i, j, ftemp);
-				} else if (i/6 == j/6) {
+				} else if (i/8 == j/8) {
 					double ftemp = 0;
-					for(k=(i%6); k<6*N; k+=6)
+					for(k=(i%8); k<8*N; k+=8)
 					{
-						if (i/6!=k/6)
+						if (i/8!=k/8)
 						{
-							double r = sqrt(
-											(y[6*(k/6)] - y[6*(i/6)]) * (y[6*(k/6)] - y[6*(i/6)]) +
-											(y[6*(k/6)+2] - y[6*(i/6)+2]) * (y[6*(k/6)+2] - y[6*(i/6)]+2) +
-											(y[6*(k/6)+4] - y[6*(i/6)+4]) * (y[6*(k/6)+4] - y[6*(i/6)]+4) );
+							double x[3];
+							x[0] = y[8*(k/8)] - y[8*(i/8)];
+							x[1] = y[8*(k/8)+3] - y[8*(i/8)+3];
+							x[2] = y[8*(k/8)+6] - y[8*(i/8)+6];
+							double r = sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
 							ftemp -= 3 * M[j/6] * (y[k-1]-y[i-1]) * (y[6*(i/6) + j%6]-y[j]) / (r*r*r*r*r);
 						}
 					}					
 					gsl_matrix_set (m, i, j, ftemp);
-				} else if (i%6 == (j+1)%6) {
-					double r = sqrt(
-									(y[6*(j/6)] - y[6*(i/6)])*(y[6*(j/6)] - y[6*(i/6)]) +
-									(y[6*(j/6)+2] - y[6*(i/6)+2])*(y[6*(j/6)+2] - y[6*(i/6)]+2) +
-									(y[6*(j/6)+4] - y[6*(i/6)+4])*(y[6*(j/6)+4] - y[6*(i/6)]+4) );
-					gsl_matrix_set (m, i, j, (M[j/6]/(r*r*r)) * (3*(y[j]-y[i-1])*(y[j]-y[i-1])/(r*r) - y[j]));
+				} else if (i%8 == (j+1)%8) {
+						double x[3];
+						x[0] = y[8*(j/8)] - y[8*(i/8)];
+						x[1] = y[8*(j/8)+3] - y[8*(i/8)+3];
+						x[2] = y[8*(j/8)+6] - y[8*(i/8)+6];
+						double r = sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
+						gsl_matrix_set (m, i, j, (M[j/8]/(r*r*r)) * (3*(y[j]-y[i-1])*(y[j]-y[i-1])/(r*r) - y[j]));
 				} else {
-					double r = sqrt(
-									(y[6*(j/6)] - y[6*(i/6)])*(y[6*(j/6)] - y[6*(i/6)]) +
-									(y[6*(j/6)+2] - y[6*(i/6)+2])*(y[6*(j/6)+2] - y[6*(i/6)]+2) +
-									(y[6*(j/6)+4] - y[6*(i/6)+4])*(y[6*(j/6)+4] - y[6*(i/6)]+4) );
-					gsl_matrix_set (m, i, j, (M[j/6]/(r*r*r)) * (3*(y[6*(j/6) + (i-1)%6]-y[i-1])*(y[j]-y[6*(i/6) + j%6])/(r*r)));
+					double x[3];
+					x[0] = y[8*(j/8)] - y[8*(i/8)];
+					x[1] = y[8*(j/8)+3] - y[8*(i/8)+3];
+					x[2] = y[8*(j/8)+6] - y[8*(i/8)+6];
+					double r = sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);
+					gsl_matrix_set (m, i, j, (M[j/8]/(r*r*r)) * (3*(y[8*(j/8) + (i-1)%8]-y[i-1])*(y[j]-y[8*(i/8) + j%8])/(r*r)));
 				}
 			}
 		}
@@ -133,21 +138,21 @@ jac (double t, const double y[], double *dfdy,
 	
 	
 	
-	for (i=0; i<6*N; i++)
+	for (i=0; i<8*N; i++)
 	{
 		dfdt[i] = 0.0;
 	}
 	
 	return GSL_SUCCESS;
 }
-*/
+
 
 int
 main (void)
 {	
 	const gsl_odeiv_step_type * T 
-		= gsl_odeiv_step_rkf45;
-		//= gsl_odeiv_step_bsimp;
+		//= gsl_odeiv_step_rkf45;
+		= gsl_odeiv_step_bsimp;
 		
 	gsl_odeiv_step * s 
 		= gsl_odeiv_step_alloc (T, 8*N);
@@ -163,11 +168,11 @@ main (void)
 	M[0] = G;
 	M[1] = G/328900.56;
 		
-	//gsl_odeiv_system sys = {func, jac, 8*N, M};
-	gsl_odeiv_system sys = {func, NULL, 8*N, M};
+	gsl_odeiv_system sys = {func, jac, 8*N, M};
+	//gsl_odeiv_system sys = {func, NULL, 8*N, M};
 	
 	double t = 0.0, t1 = 365.25;
-	double h0 = 1E-1;
+	double h0 = 1E2;
 	double h = h0;
 	double sp = 0.0;
 	double y[8*N] = { 0.0, 0.0, sp, 0.0, 0.0, sp, 0.0, 0.0, 1, 0.0, sp, 0.0, k, sp, 0.0, 0.0 };
@@ -185,7 +190,7 @@ main (void)
 			break;
 		
 		printf("%f", t);
-		h = h0;
+		//h = h0;
 		int i;
 		for(i = 0; i < 8*N; i++) {
 			if (i!=2 && i!=5)
